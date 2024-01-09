ecea91112bafed7a3eb23cd76d037489debefe8e
Ravish Patel <ravish@sysotel.ai>
2024-01-09 19:27:40 +0530

    update

--- src/DB/MongoODM/Repositories/PromotionsRepository.php
+++ src/DB/MongoODM/Repositories/PromotionsRepository.php
@@ -6,5 +6,46 @@ use Delta4op\MongoODM\DocumentRepositories\DocumentRepository;
 
 class PromotionsRepository extends DocumentRepository
 {
+    /**
+     * @param int|Property $property
+     * @param array $criteria
+     * @param array $sort
+     * @return Collection
+     */
+    public function getActiveAndDisableForProperty(int|Property $property, array $criteria = [], array $sort = []): Collection
+    {
 
+        $criteria = array_merge(
+            ['propertyID' => Property::resolveID($property),
+                'isExpired' => false],
+            $criteria);
+
+
+
+        $sort = array_merge([
+            'createdAt' => -1
+        ], $sort);
+
+        return $this->getCollectionBy($criteria, $sort);
+    }
+
+    /**
+     * @param int $promoID
+     * @param string $type
+     * @param array $criteria
+     * @param array $sort
+     * @return Promotion|null
+     */
+    public function findLatestBy(int $promoID, string $type, array $criteria = [], array $sort = []): null|Promotion
+    {
+        $criteria = array_merge([
+            'promoID' => $promoID, 'type' => $type, 'isExpired' => false
+        ], $criteria);
+
+        $sort = array_merge([
+            'createdAt' => -1
+        ], $sort);
+
+        return $this->findOneBy($criteria, $sort);
+    }
 }
