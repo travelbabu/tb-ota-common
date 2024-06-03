2572a5f4730c9f8dd2c3ab8c7a6b35a2224d865a
Ravish <gitwithravish@gmail.com>
2024-06-03 14:21:37 +0530

    update

--- src/DB/MongoODM/Documents/Attributes/Dedicated/StandardRestrictionAttributes.php
+++ src/DB/MongoODM/Documents/Attributes/Dedicated/StandardRestrictionAttributes.php
@@ -99,22 +99,27 @@ class StandardRestrictionAttributes extends EmbeddedDocument
     {
         if ($this->stopSell?->value === null) {
             $this->stopSell = $attributes->stopSell;
+            $this->stopSell->isUpdated = false;
         }
 
         if ($this->closeOnArrival?->value === null) {
             $this->closeOnArrival = $attributes->closeOnArrival;
+            $this->closeOnArrival->isUpdated = false;
         }
 
         if ($this->closeOnDeparture?->value === null) {
             $this->closeOnDeparture = $attributes->closeOnDeparture;
+            $this->closeOnDeparture->isUpdated = false;
         }
 
         if ($this->minLos?->value === null) {
             $this->minLos = $attributes->minLos;
+            $this->minLos->isUpdated = false;
         }
 
         if ($this->maxLos?->value === null) {
             $this->maxLos = $attributes->maxLos;
+            $this->maxLos->isUpdated = false;
         }
 
         return $this;
