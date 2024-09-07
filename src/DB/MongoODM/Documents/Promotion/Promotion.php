6e6058c1fdbea4c170969b8fb5dbcda235caf62f
Ravish <gitwithravish@gmail.com>
2024-09-07 16:29:54 +0530

    update

--- src/DB/MongoODM/Documents/Promotion/Promotion.php
+++ src/DB/MongoODM/Documents/Promotion/Promotion.php
@@ -7,8 +7,6 @@ use Delta4op\MongoODM\Documents\Document;
 use Delta4op\MongoODM\Facades\DocumentManager;
 use Delta4op\MongoODM\Traits\HasDefaultAttributes;
 use Delta4op\MongoODM\Traits\HasTimestamps;
-use Doctrine\Common\Collections\ArrayCollection;
-use SYSOTEL\OTA\Common\DB\MongoODM\Documents\Promotion\embedded\PromotionApplicableSpace;
 use SYSOTEL\OTA\Common\DB\MongoODM\Documents\Promotion\embedded\PromotionApplicableSpaceDetails;
 use SYSOTEL\OTA\Common\DB\MongoODM\Repositories\PromotionsRepository;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
@@ -44,10 +42,22 @@ class Promotion extends Document
     public $id;
 
     /**
+     * @var ?string
+     * @ODM\Field(type="string")
+     */
+    public $category;
+
+    /**
      * @var ?int
      * @ODM\Field(type="int")
      */
     public $promoID;
+
+    /**
+     * @var ?string
+     * @ODM\Field(type="string")
+     */
+    public $promoCode;
 
     /**
      * @var ?int
--- src/DB/MongoODM/Repositories/PromotionsRepository.php
+++ src/DB/MongoODM/Repositories/PromotionsRepository.php
@@ -3,12 +3,75 @@
 namespace SYSOTEL\OTA\Common\DB\MongoODM\Repositories;
 
 use Illuminate\Support\Collection;
+use SYSOTEL\OTA\Common\Helpers\Enums;
 use Delta4op\MongoODM\DocumentRepositories\DocumentRepository;
 use SYSOTEL\OTA\Common\DB\MongoODM\Documents\Property\Property;
 use SYSOTEL\OTA\Common\DB\MongoODM\Documents\Promotion\Promotion;
 
 class PromotionsRepository extends DocumentRepository
 {
+    public function findOfferById(string $id): Promotion|null
+    {
+        return $this->findOneBy([
+            '_id' => $id,
+            'category' => Enums::PROMOTION_CATEGORY_OFFER
+        ]);
+    }
+
+    public function findPromoCodeById(string $id): Promotion|null
+    {
+        return $this->findOneBy([
+            '_id' => $id,
+            'category' => Enums::PROMOTION_CATEGORY_PROMO_CODE
+        ]);
+    }
+
+    /**
+     * @param int|Property $property
+     * @param array $criteria
+     * @param array $sort
+     * @return Collection<Promotion>
+     */
+    public function getAllOffersForProperty(int|Property $property, array $criteria = [], array $sort = []): Collection
+    {
+        $criteria = array_merge(
+            [
+                'propertyID' => Property::resolveID($property),
+                'category' => Enums::PROMOTION_CATEGORY_OFFER,
+                'isExpired' => false,
+            ],
+            $criteria);
+
+        $sort = array_merge([
+            'createdAt' => -1
+        ], $sort);
+
+        return $this->getCollectionBy($criteria, $sort);
+    }
+
+    /**
+     * @param int|Property $property
+     * @param array $criteria
+     * @param array $sort
+     * @return Collection<Promotion>
+     */
+    public function getAllPromoCodesForProperty(int|Property $property, array $criteria = [], array $sort = []): Collection
+    {
+        $criteria = array_merge(
+            [
+                'propertyID' => Property::resolveID($property),
+                'category' => Enums::PROMOTION_CATEGORY_PROMO_CODE,
+                'isExpired' => false,
+            ],
+            $criteria);
+
+        $sort = array_merge([
+            'createdAt' => -1
+        ], $sort);
+
+        return $this->getCollectionBy($criteria, $sort);
+    }
+
     /**
      * @param int|Property $property
      * @param array $criteria
@@ -20,6 +83,54 @@ class PromotionsRepository extends DocumentRepository
         $criteria = array_merge(
             [
                 'propertyID' => Property::resolveID($property),
+                'isExpired' => false
+            ],
+            $criteria);
+
+        $sort = array_merge([
+            'createdAt' => -1
+        ], $sort);
+
+        return $this->getCollectionBy($criteria, $sort);
+    }
+
+    /**
+     * @param int|Property $property
+     * @param array $criteria
+     * @param array $sort
+     * @return Collection<Promotion>
+     */
+    public function getActiveOffersForProperty(int|Property $property, array $criteria = [], array $sort = []): Collection
+    {
+        $criteria = array_merge(
+            [
+                'propertyID' => Property::resolveID($property),
+                'status' => Promotion::STATUS_ACTIVE,
+                'category' => Enums::PROMOTION_CATEGORY_OFFER,
+                'isExpired' => false
+            ],
+            $criteria);
+
+        $sort = array_merge([
+            'createdAt' => -1
+        ], $sort);
+
+        return $this->getCollectionBy($criteria, $sort);
+    }
+
+    /**
+     * @param int|Property $property
+     * @param array $criteria
+     * @param array $sort
+     * @return Collection<Promotion>
+     */
+    public function getActivePromoCodesForProperty(int|Property $property, array $criteria = [], array $sort = []): Collection
+    {
+        $criteria = array_merge(
+            [
+                'propertyID' => Property::resolveID($property),
+                'status' => Promotion::STATUS_ACTIVE,
+                'category' => Enums::PROMOTION_CATEGORY_PROMO_CODE,
                 'isExpired' => false
             ],
             $criteria);
@@ -52,6 +163,52 @@ class PromotionsRepository extends DocumentRepository
         ], $sort);
 
         return $this->getCollectionBy($criteria, $sort);
+    }
+
+    /**
+     * @param int $promoID
+     * @param string $type
+     * @param array $criteria
+     * @param array $sort
+     * @return Promotion|null
+     */
+    public function findLatestOfferByPromoId(int $promoID, string $type, array $criteria = [], array $sort = []): null|Promotion
+    {
+        $criteria = array_merge([
+            'promoID' => $promoID,
+            'type' => $type,
+            'isExpired' => false,
+            'category' => Enums::PROMOTION_CATEGORY_OFFER,
+        ], $criteria);
+
+        $sort = array_merge([
+            'createdAt' => -1
+        ], $sort);
+
+        return $this->findOneBy($criteria, $sort);
+    }
+
+    /**
+     * @param int $promoID
+     * @param string $type
+     * @param array $criteria
+     * @param array $sort
+     * @return Promotion|null
+     */
+    public function findLatestPromoCodeByPromoId(int $promoID, string $type, array $criteria = [], array $sort = []): null|Promotion
+    {
+        $criteria = array_merge([
+            'promoID' => $promoID,
+            'type' => $type,
+            'isExpired' => false,
+            'category' => Enums::PROMOTION_CATEGORY_PROMO_CODE,
+        ], $criteria);
+
+        $sort = array_merge([
+            'createdAt' => -1
+        ], $sort);
+
+        return $this->findOneBy($criteria, $sort);
     }
 
     /**
