6bf07c2c67a7e49d304222df2cff1fd814d1af59
Ravish <gitwithravish@gmail.com>
2024-09-23 23:54:34 +0530

    update

--- src/Enums/ReviewCategory.php
+++ src/Enums/ReviewCategory.php
@@ -6,23 +6,23 @@ use function SYSOTEL\OTA\Common\Helpers\readableConstant;
 
 enum ReviewCategory: string
 {
+    case STAFF = 'STAFF';
+    case FACILITIES = 'FACILITIES';
     case SERVICE = 'SERVICE';
     case CLEANLINESS = 'CLEANLINESS';
-    case SLEEP_QUALITY = 'SLEEP_QUALITY';
     case LOCATION = 'LOCATION';
-    case SPACE = 'SPACE';
-    case BREAKFAST = 'BREAKFAST';
-    case VALUE = 'VALUE';
-    case SPA = 'SPA';
-    case ROOM = 'ROOM';
     case COMFORT = 'COMFORT';
-    case FACILITIES = 'FACILITIES';
-    case STAFF = 'STAFF';
     case VALUE_FOR_MONEY = 'VALUE_FOR_MONEY';
-    case FREE_WIFI = 'FREE_WIFI';
-    case FOOD_DRINK = 'FOOD_DRINK';
-    case HOTEL_CONDITION = 'HOTEL_CONDITION';
-    case POOL = 'POOL';
+//    case SLEEP_QUALITY = 'SLEEP_QUALITY';
+//    case SPACE = 'SPACE';
+//    case BREAKFAST = 'BREAKFAST';
+//    case VALUE = 'VALUE';
+//    case SPA = 'SPA';
+//    case ROOM = 'ROOM';
+//    case FREE_WIFI = 'FREE_WIFI';
+//    case FOOD_DRINK = 'FOOD_DRINK';
+//    case HOTEL_CONDITION = 'HOTEL_CONDITION';
+//    case POOL = 'POOL';
 
     /**
      * @return string
