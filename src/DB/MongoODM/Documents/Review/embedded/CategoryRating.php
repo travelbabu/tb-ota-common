ae1794c5446ede80689a8dda48ff69e16d0e9a03
Ravish <gitwithravish@gmail.com>
2024-09-25 00:17:50 +0530

    update

--- src/DB/MongoODM/Documents/Review/embedded/CategoryRating.php
+++ src/DB/MongoODM/Documents/Review/embedded/CategoryRating.php
@@ -35,9 +35,19 @@ class CategoryRating extends EmbeddedDocument
      */
     public $score;
 
+    public function calculateScore(): float|int|null
+    {
+        if ($this->ratingTopLine !== null && $this->ratingGiven !== null) {
+            // Ensure that ratingGiven is within the range of 0 to ratingTopLine
+            $this->ratingGiven = max(0, min($this->ratingGiven, $this->ratingTopLine));
+            return $this->score = (int)(($this->ratingGiven / $this->ratingTopLine) * 100);
+        } else {
+            return  $this->score = null;
+        }
+    }
     /**
      * @inheritDoc
-    */
+     */
     public function toArray(): array
     {
         return arrayFilter([
