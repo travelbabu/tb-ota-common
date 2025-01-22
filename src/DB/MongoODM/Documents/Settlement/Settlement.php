fdb8af69ce190a48b66dd9f6dbfd81d1ac7391e5
travelbabu <travelbabumanager@gmail.com>
2025-01-23 03:39:21 +0530

    update

--- src/DB/MongoODM/Documents/Settlement/Settlement.php
+++ src/DB/MongoODM/Documents/Settlement/Settlement.php
@@ -14,7 +14,7 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
  *     collection="settlements",
  * )
  * @ODM\HasLifecycleCallbacks
- * @ODM\InheritanceType("type")
+ * @ODM\InheritanceType("SINGLE_COLLECTION")
  * @ODM\DiscriminatorField("type")
  * @ODM\DiscriminatorMap({
  *     "BOOKING_PROPERTY":SYSOTEL\OTA\Common\DB\MongoODM\Documents\Settlement\PropertyBooking\PropertySettlementCalculations::class,
