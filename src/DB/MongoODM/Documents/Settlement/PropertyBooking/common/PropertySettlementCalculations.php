1c649600c3ba3edee4208048d1f488d45c059a8b
Ravish <gitwithravish@gmail.com>
2025-01-22 16:46:12 +0530

    settlement documents added

--- src/DB/MongoODM/Documents/Settlement/PropertyBooking/common/PropertySettlementCalculations.php
+++ src/DB/MongoODM/Documents/Settlement/PropertyBooking/common/PropertySettlementCalculations.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SYSOTEL\OTA\Common\DB\MongoODM\Documents\Settlement\common;
+namespace SYSOTEL\OTA\Common\DB\MongoODM\Documents\SettlementPropertyBooking\common;
 
 use Delta4op\MongoODM\Documents\EmbeddedDocument;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
