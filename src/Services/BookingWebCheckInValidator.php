61bd95c3a903e6f00a815b75312a05cf9d4fc5b3
travelbabu <travelbabumanager@gmail.com>
2025-02-08 19:07:53 +0530

    update

---
+++ src/Services/BookingWebCheckInValidator.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SYSOTEL\OTA\Common\Services;
+
+use SYSOTEL\OTA\Common\DB\MongoODM\Documents\Booking\Booking;
+use SYSOTEL\OTA\Common\Helpers\Enums;
+
+class BookingWebCheckInValidator
+{
+    protected Booking $booking;
+
+    public function __construct(Booking $booking)
+    {
+        $this->booking = $booking;
+    }
+
+    /**
+     * @return array{
+     *     checkInAllowed: boolean,
+     *     errorDescription: string
+     * }
+    */
+    public function inspect(): array
+    {
+        $response = [
+            'checkInAllowed' => true,
+            'errorDescription' => ''
+        ];
+
+        if(today()->gt($this->booking->stayDates->checkInDate)) {
+            $response['errorDescription'] = 'Web check in is not after date of check in';
+        }
+
+        else if ($this->booking->status === Enums::BOOKING_STATUS_CANCELLED) {
+            $response['errorDescription'] = 'Web check in is not allowed for cancelled booking';
+        }
+
+        else if (!in_array($this->booking->status, [Enums::BOOKING_STATUS_CONFIRMED, Enums::BOOKING_STATUS_MODIFIED])) {
+            $response['errorDescription'] = 'Web check in is not allowed for this booking';
+        }
+
+        if($response['errorDescription']) {
+            $response['checkInAllowed'] = false;
+        }
+
+        return $response;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isWebCheckInAllowed(): bool
+    {
+        return $this->inspect()['checkInAllowed'] === true;
+    }
+}
